ad37fb7918c0b4c4d8cd6f133601a7c7ef4342e7
jiang-alincoln <152939797+jiang-alincoln@users.noreply.github.com>
2025-08-27 15:18:23 -0700

    Unit 1 time for the plane project

--- Junior Programming Pathway/Assets/Scripts/PlayerController.cs
+++ Junior Programming Pathway/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    private float speed = 20.0f;
+    private float turnSpeed = 90.0f;
+    public float horizontalInput;
+    public float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,7 +15,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         // Move the vehicle forward
-        transform.Translate(Vector3.forward*Time.deltaTime *20);
+        transform.Translate(Vector3.forward*Time.deltaTime *speed*forwardInput);
+        transform.Rotate(Vector3.up,Time.deltaTime*turnSpeed*horizontalInput);
     }
 }
